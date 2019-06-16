7d6642893a764aae50e8de441349cfef472f37a2
unknown <sahal737@gmail.com>
2019-06-16 19:26:40 +0530

    9thcommit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  getUrl(){
+    return 'url(https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80)'
+  }
 }
